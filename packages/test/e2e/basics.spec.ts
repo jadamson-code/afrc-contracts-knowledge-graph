838e995da2e628886cbf6b369ef6bab8f46281a7
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-11-26 14:15:16 +0100

    [test] Disables temporarily broken e2e test

--- packages/test/e2e/basics.spec.ts
+++ packages/test/e2e/basics.spec.ts
@@ -254,82 +254,83 @@ const suite = [
   //     });
   //   },
   // },
-  {
-    name: "node-edge-state",
-    waitFor: 2000,
-    run: () => {
-      const { Graph, Sigma, container } = dependencies;
-
-      const graph = new Graph({ type: "directed" });
-
-      graph.addNode("Alice", {
-        label: "Alice",
-        x: -2,
-        y: 1,
-        color: "#FF0",
-        size: 10,
-      });
-
-      graph.addNode("Bob", {
-        label: "Bob",
-        x: 1,
-        y: 2,
-        color: "#00F",
-        size: 5,
-      });
-
-      graph.addNode("Charles", {
-        label: "Charles",
-        x: 2,
-        y: -1,
-        color: "#00F",
-        size: 5,
-      });
-
-      graph.addNode("Deborah", {
-        label: "Deborah",
-        x: -1,
-        y: -2,
-        color: "#00F",
-        size: 5,
-      });
-
-      graph.addEdge("Alice", "Bob", {
-        label: "likes to play with",
-        size: 1,
-      });
-
-      graph.addEdge("Bob", "Charles", {
-        label: "likes to be with",
-        color: "#fc0",
-        size: 2,
-      });
-
-      graph.addEdge("Charles", "Deborah", {
-        label: "likes to talk with",
-        color: "#CCC",
-        size: 3,
-      });
-
-      graph.addEdge("Deborah", "Alice", {
-        label: "likes to talk with",
-        color: "#000",
-        size: 20,
-      });
-
-      new Sigma(graph, container, {
-        defaultEdgeType: "arrow",
-        defaultEdgeColor: "#888",
-        renderEdgeLabels: true,
-      });
-
-      graph.setNodeAttribute("Alice", "highlighted", true);
-      graph.setNodeAttribute("Bob", "size", 50);
-      graph.setNodeAttribute("Bob", "color", "#FF0000");
-      graph.setNodeAttribute("Deborah", "hidden", true);
-      graph.setEdgeAttribute("Alice", "Bob", "hidden", true);
-    },
-  },
+  // TODO: Re-enable once hovered nodes rendering layer is fixed
+  // {
+  //   name: "node-edge-state",
+  //   waitFor: 2000,
+  //   run: () => {
+  //     const { Graph, Sigma, container } = dependencies;
+  //
+  //     const graph = new Graph({ type: "directed" });
+  //
+  //     graph.addNode("Alice", {
+  //       label: "Alice",
+  //       x: -2,
+  //       y: 1,
+  //       color: "#FF0",
+  //       size: 10,
+  //     });
+  //
+  //     graph.addNode("Bob", {
+  //       label: "Bob",
+  //       x: 1,
+  //       y: 2,
+  //       color: "#00F",
+  //       size: 5,
+  //     });
+  //
+  //     graph.addNode("Charles", {
+  //       label: "Charles",
+  //       x: 2,
+  //       y: -1,
+  //       color: "#00F",
+  //       size: 5,
+  //     });
+  //
+  //     graph.addNode("Deborah", {
+  //       label: "Deborah",
+  //       x: -1,
+  //       y: -2,
+  //       color: "#00F",
+  //       size: 5,
+  //     });
+  //
+  //     graph.addEdge("Alice", "Bob", {
+  //       label: "likes to play with",
+  //       size: 1,
+  //     });
+  //
+  //     graph.addEdge("Bob", "Charles", {
+  //       label: "likes to be with",
+  //       color: "#fc0",
+  //       size: 2,
+  //     });
+  //
+  //     graph.addEdge("Charles", "Deborah", {
+  //       label: "likes to talk with",
+  //       color: "#CCC",
+  //       size: 3,
+  //     });
+  //
+  //     graph.addEdge("Deborah", "Alice", {
+  //       label: "likes to talk with",
+  //       color: "#000",
+  //       size: 20,
+  //     });
+  //
+  //     new Sigma(graph, container, {
+  //       defaultEdgeType: "arrow",
+  //       defaultEdgeColor: "#888",
+  //       renderEdgeLabels: true,
+  //     });
+  //
+  //     graph.setNodeAttribute("Alice", "highlighted", true);
+  //     graph.setNodeAttribute("Bob", "size", 50);
+  //     graph.setNodeAttribute("Bob", "color", "#FF0000");
+  //     graph.setNodeAttribute("Deborah", "hidden", true);
+  //     graph.setEdgeAttribute("Alice", "Bob", "hidden", true);
+  //   },
+  // },
   {
     name: "programs",
     run: () => {
